bc905e564ca1c01d3fb8b846f5dea77e816f31c2
Mitsuru Andoh <andoh1961@gmail.com>
2025-10-28 15:30:28 +0900

    ToDo/ToDoDetailルートの設定

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\ToDoController;
+use App\Http\Controllers\ToDoDetailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('todo')->group(function(){
+    Route::resource('/detail', ToDoDetailController::class);
+});
+Route::resource('/todo', ToDoController::class);
+
