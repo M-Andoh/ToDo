f202e15db1c7f3b1b5567ef9692e031d3e535b0b
Mitsuru Andoh <andoh1961@gmail.com>
2025-11-03 08:40:16 +0900

    ### ディレクトリ名変更

--- resources/ts/hocks/TodoList/useCurrentToList.ts
+++
@@ -1,9 +0,0 @@
-import { useQueryClient } from "@tanstack/react-query";
-import type { ToDoType } from "../../types/ToDoTypes.js";
-
-const useCurrentToDoList = () => {
-    const queryClient = useQueryClient();
-    return queryClient.getQueryData<ToDoType[]>(["todo"]);
-}
-
-export default useCurrentToDoList;
